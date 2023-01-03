7314565ed301e62d37460bae79caebf5beb9cab0
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-03 08:56:27 -0800

    Init

---
+++ src/component/Counter.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export class Counter extends React.Component {
+    constructor(props) {
+        console.log("constructor");
+        super(props);
+
+        this.state = {
+            counter: 0,
+        }
+
+        this.increment = () => this.setState({counter: this.state.counter + 1});
+        this.decrement = () => this.setState({counter: this.state.counter - 1});
+    }
+
+    componentDidMount() {
+        console.log("Component Did Mount");
+        console.log("---------------------");
+    }
+
+    render() {
+        console.log('Render');
+
+        return (
+            <div>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Deccrement</button>
+                <div
+                    className="counter"
+                >
+                    Counter: {this.state.counter}
+                </div>
+            </div> 
+        )
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("Component Did Update");
+        console.log("---------------------");
+    }
+
+    componentWillUnmount() {
+        console.log("Component Will Unmount");
+        console.log("---------------------");
+    }
+}
